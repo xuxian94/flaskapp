ac416369a4df9c086a79aadfec3c4aea751cba98
xuxian <876438057@qq.com>
2017-07-19 11:06:44 +0800

    Revert "个人简介页面1"
    This reverts commit 3fee9b1f192e83765029db422814c7f9f53e36d9.
    
    # Conflicts:
    #	flaskapp/order/models.py
    #	flaskapp/templates/order/base.html

--- flaskapp/order/models.py
+++ flaskapp/order/models.py
@@ -10,7 +10,6 @@ from jieba.analyse.analyzer import ChineseAnalyzer
 
 class Sheet_Form(db.Model):
     __tablename__ = 'info_jizhuan'
-
     __searchable__ = ['company', 'address']
     __analyzer__ = ChineseAnalyzer()
 
@@ -108,12 +107,14 @@ def search_engine(content, page):
 
 def select_address_checkbox(page):
     """
-    
-    :param page: 
-    :return: 
+    地区复选框搜索功能
+    :param page: 页数
+    :return: 分页后的查询结果
     """
-    info_address=request.values.getlist("address")
-    # naginations = []
+
+
+    info_address = request.values.getlist("address")
+    naginations = []
 
     # for info_address in info_address_all:
     #     info=Sheet_Form.query.filter(Sheet_Form.address.like('%'+info_address+'%'))
