add011faa29f68b57e145f10eca52a2a3050189e
guoXiaoru <405366877@qq.com>
2017-07-25 16:54:17 +0800

    机构样式

--- flaskapp/public/views.py
+++ flaskapp/public/views.py
@@ -61,3 +61,23 @@ def about():
     """About page."""
     form = LoginForm(request.form)
     return render_template('public/about.html', form=form)
+
+
+@blueprint.route('/login',methods=['GET', 'POST'])
+def login():
+    """
+    用户登录页面
+    :return: 
+    """
+    form = LoginForm(request.form)
+    # Handle logging in
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            login_user(form.user)
+            flash('You are logged in.', 'success')
+
+            redirect_url = request.args.get('next') or url_for('user.members')
+            return redirect(redirect_url)
+        else:
+            flash_errors(form)
+    return render_template('public/login.html', form=form)
