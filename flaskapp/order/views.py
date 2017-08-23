801b1eee94c4824580b809ee98731dda21d531eb
guoXiaoru <405366877@qq.com>
2017-08-23 08:48:26 +0800

    version1.0_1

--- flaskapp/order/views.py
+++ flaskapp/order/views.py
@@ -4,122 +4,93 @@ from flask import render_template, Blueprint
 from flask_login import login_required
 from models import *
 
-
-
 blueprint = Blueprint('order', __name__, static_folder='../static/order')
-# Basic_Info_Form = Basic_Info_Form()
-# Avator = Avator()
 
 
-@blueprint.route('/index/<int:page>')
+@blueprint.route('/searchbox1/<int:page>')
 @login_required
-def blank(page):
+def searchbox1(page):
     """
-        路由：index.html
-        初始主页，即分页第一页
-        分页显示了所有数据
+    用户自定义搜索，根据专家名称进行模糊搜索
+    :return: 专家搜索结果的陈列页面
     """
-    # Professor.get_many(11, 1589, 'flaskapp/static/order/avator').all()
-    pagination = basic_info.query.paginate(page=page, per_page=6, error_out=False)
-    return render_template('order/index.html', title='三螺旋', pagination=pagination)
+    basic_info = Basic_info()
+    expert_name = request.values.get('condition1')
+    if not expert_name:
+        expert_name = ""
+    pagination = basic_info.search_box(expert_name).paginate(page=page, per_page=6, error_out=False)
+    return render_template('order/index.html', title='三螺旋', expert_name=expert_name, pagination=pagination)
 
-
-# @blueprint.route('/searchbox')
+# @blueprint.route('/order_confirm')
 # @login_required
-# def searchbox():
+# def about():
 #     """
-#     用户自定义搜索，主要是根据专家（导师）和学校搜索
-#     :return: 专家陈列页面，指定学校陈列页
+#         路由：order_confirm.html
+#         跳转订单确认页面
 #     """
-#     info_text = select_box()
-#     return render_template('order/search.html', title='三螺旋', pagination=info_text, con=info_text)
+#     return render_template('order/order_confirm.html')
 
 
-# @blueprint.route('/insti_search/<int:page>')
+# @blueprint.route('/order_list')
 # @login_required
-# def insti_search(page):
+# def order():
 #     """
-#     对机构信息的查看
-#     :return:
+#         路由：order_list.html
+#         跳转订单列表页面
 #     """
-#     if not page:
-#         page = 1
-#     lab_info = Lab_Form()
-#     pagination = lab_info.select_info().paginate(page=page, per_page=6, error_out=False)
-#     return render_template('laboratory/lab_index.html', title='三螺旋', pagination=pagination)
+#     return render_template('order/order_list.html')
 
 
-@blueprint.route('/search/<int:page>')
-@login_required
-def search(page):
-    """
-    :param page: 结果从第几页展示
-    :return: 根据条件进行模糊匹配的结果陈列
-    """
-    if not page:
-        page = 1
-    info_address_list, info_address = Basic_Info_Form.select_address_radio(page=page)
-    print info_address_list
-    return render_template('order/search.html', title='三螺旋', pagination=info_address_list, address=info_address)
+# @blueprint.route('/<string:con>/<int:page>')
+# @login_required
+# def add(con, page):
+#     """
+#         路由：add.html
+#         按地址或公司名查询
+#         返回结果在add.html中显示
+#     """
+#     # pagination = search_engine(area, page)
+#     pagination = select_paginate_by_add(con, page)
+#     return render_template('order/add.html', title="三螺旋", pagination=pagination, name=con)
 
 
-
-
-
-@blueprint.route('/order_confirm')
-@login_required
-def about():
-    """
-        路由：order_confirm.html
-        跳转订单确认页面
-    """
-    return render_template('order/order_confirm.html')
-
-
-@blueprint.route('/order_list')
-@login_required
-def order():
-    """
-        路由：order_list.html
-        跳转订单列表页面
-    """
-    return render_template('order/order_list.html')
-
-
-@blueprint.route('/<string:con>/<int:page>')
-@login_required
-def add(con, page):
-    """
-        路由：add.html
-        按地址或公司名查询
-        返回结果在add.html中显示
-    """
-    # pagination = search_engine(area, page)
-    pagination = select_paginate_by_add(con, page)
-    return render_template('order/add.html', title="三螺旋", pagination=pagination, name=con)
-
-
-@blueprint.route('/demand')
-@login_required
-def demand():
-    """
-        路由：
-        触发搜索
-        返回add方法
-    """
-    content = request.args.get('requirement')
-    return add(content, 1)
-
+# @blueprint.route('/demand')
+# @login_required
+# def demand():
+#     """
+#         路由：
+#         触发搜索
+#         返回add方法
+#     """
+#     content = request.args.get('requirement')
+#     return add(content, 1)
 
 @blueprint.route('/profile/<int:id>')
 @login_required
 def profile(id):
-    info = Professor(id)
+    info = Professor(id, 'avator/')
     return render_template('order/profile.html', title="三螺旋", info=info)
+
+
+@blueprint.route('/searchbox2/<int:page>')
+@login_required
+def searchbox2(page):
+    """
+    用户自定义搜索，根据重点实验室名称进行模糊搜索
+    :return: 实验室陈列页面
+    """
+    lab_info = Lab_Form()
+    lab_name = request.values.get('condition2')
+    if not lab_name:
+        lab_name = ""
+    pagination = lab_info.search_box(lab_name).paginate(page=page, per_page=6, error_out=False)
+    return render_template('laboratory/lab_search.html', title='三螺旋', lab_name = lab_name, pagination=pagination)
 
 @blueprint.route('/lab_infoshow/<string:name>')
 def lab_infoshow(name):
     labform = Lab_Form()
     info = labform.get_info(name=name)
     return render_template('laboratory/lab_infoshow.html', title="三螺旋", info=info)
+
+
 
